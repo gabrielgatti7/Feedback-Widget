35c05b42058966f5dc157f091571a836957d4774
Gabriel <gabrielgatti27@gmail.com>
2025-04-02 20:42:24 -0300

    :sparkles: add initial back-end: express and prisma configs, feedback post route

---
+++ server/src/server.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import { prisma } from "./prisma";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/feedbacks", async (req, res) => {
+  const feedback = await prisma.feedback.create({
+    data: {
+      type: req.body.type,
+      comment: req.body.comment,
+      screenshot: req.body.screenshot,
+    }
+  });
+
+  // status 201 means "Created"
+  res.status(201).json(feedback);
+})
+
+app.listen(3333, () => {
+  console.log("HTTP Server started on port 3333");
+});
