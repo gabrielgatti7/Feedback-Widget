feb8e0c9a50e51456c9b3e7dff804ade0fd30ce7
Gabriel <gabrielgatti27@gmail.com>
2025-04-16 15:19:22 -0300

    :bug: fix environment variable problem

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express from "express"
 import { routes } from "./routes";
+import 'dotenv/config';
 
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
